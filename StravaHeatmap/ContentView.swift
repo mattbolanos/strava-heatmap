22a91ae229e387a8101b11b31296050e7267549e
mattbolanos <matthew.a.bolanos@gmail.com>
2026-02-20 15:50:11 -0500

    Initial Commit

---
+++ StravaHeatmap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StravaHeatmap
+//
+//  Created by Matt Bolaños on 2/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StravaHeatmap/StravaHeatmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravaHeatmapApp.swift
+//  StravaHeatmap
+//
+//  Created by Matt Bolaños on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct StravaHeatmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
